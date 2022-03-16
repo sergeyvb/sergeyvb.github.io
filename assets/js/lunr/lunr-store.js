20ed8f94c40d93f50abc33e861d768fd0c473991
sergeyvb <bukov85@mail.ru>
2022-03-16 22:14:29 +0500

    test ver.0.2

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Quick-Start Guide",
+        "excerpt":"Minimal Mistakes has been developed as a Gem-based theme for easier use, and 100% compatible with GitHub Pages when used as a remote theme. If you enjoy this theme, please consider sponsoring me to continue developing and maintaining it. Installing the theme If you’re running Jekyll v3.7+ and self-hosting you...","categories": [],
+        "tags": [],
+        "url": "/docs/quick-start-guide/",
+        "teaser": null
+      },{
+        "title": "Edge Case: Nested and Mixed Lists",
+        "excerpt":"Nested and mixed lists are an interesting beast. It’s a corner case to make sure that Lists within lists do not break the ordered list numbering order Your list styles go deep enough. Ordered – Unordered – Ordered ordered item ordered item unordered unordered ordered item ordered item ordered item...","categories": ["Edge Case"],
+        "tags": ["content","css","edge case","lists","markup"],
+        "url": "/edge%20case/edge-case-nested-and-mixed-lists/",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "/jekyll/welcome-to-jekyll/",
+        "teaser": "/assets/images/500x300.png"
+      },]
